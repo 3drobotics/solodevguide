b55bb2f2211da0fbb7228c09c0e631b68348714f
Tim Ryan <tim@3drobotics.com>
2015-08-12 15:08:04 -0700

    Updates publish script.

--- scripts/publish-pages.sh
+++ scripts/publish-pages.sh
@@ -5,5 +5,5 @@ cd $(dirname $0)/..
 gitbook build book
 git add book/_book -f
 git commit -am "Publishes to gh-pages."
-git push origin `git subtree split --prefix book/_book master`:gh-pages --force
+git push origin `git subtree split --prefix book/_book master`:refs/heads/gh-pages --force
 git reset --hard HEAD~1
