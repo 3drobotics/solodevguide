14fe1715270e7b8b8664b4f640b854d74624efa1
Tim Ryan <tim@3drobotics.com>
2015-08-12 17:07:10 -0700

    Adds cache control

--- scripts/publish-pages.sh
+++ scripts/publish-pages.sh
@@ -3,4 +3,4 @@
 cd $(dirname $0)/..
 
 gitbook build book
-aws s3 sync book/_book/ s3://ffc6904fed514b42b88f87926328069c5c8149f4/ --acl public-read
+aws s3 sync book/_book/ s3://ffc6904fed514b42b88f87926328069c5c8149f4/ --acl public-read --cache-control "max-age=3600"
