ef67d5530591decfc71a5129d68bb6670be3b528
Tim Ryan <tim@3drobotics.com>
2015-08-28 15:12:24 -0700

    Fixes pushstate clearing markdown changes.

--- book/js/website.js
+++ book/js/website.js
@@ -1,6 +1,18 @@
-$(function () {
+function redoMarkdown () {
   $('aside').each(function () {
     var $el = $(this);
     $el.html(kramed($el.text()));
   });
-});
+}
+
+// Intercept pushstate
+(function(history){
+    var pushState = history.pushState;
+    history.pushState = function(state) {
+      setTimeout(redoMarkdown, 0);
+      return pushState.apply(history, arguments);
+    }
+})(window.history);
+
+$(redoMarkdown);
+$(window).on('popstate', redoMarkdown);
