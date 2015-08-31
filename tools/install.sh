f92a297ed7ad7eabdd60a400f0c06ac9b745fcf6
Tim Ryan <tim@3drobotics.com>
2015-08-31 14:34:39 -0700

    Updates installer script.

--- tools/install.sh
+++ tools/install.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
 # Unpack sshuttle
-echo 'unpacking sshuttle.'
-ssh root@10.1.1.10 "[[ -d /opt/sshuttle ]]"
-if [ $? != 0 ]; then
-    curl -L https://github.com/apenwarr/sshuttle/archive/master.tar.gz | ssh root@10.1.1.10 "tar -xvzf - -C /tmp && rm -rf /opt/sshuttle && mv /tmp/sshuttle-master /opt/sshuttle"
+echo 'checking for sshuttle....'
+ssh root@10.1.1.10 "[[ -d /opt/sshuttle ]]" 2>/dev/null </dev/null
+if [[ $? != 0 ]]; then
+    echo 'unpacking sshuttle.'
+    curl -L https://github.com/apenwarr/sshuttle/archive/master.tar.gz | \
+        ssh root@10.1.1.10 "tar -xvzf - -C /tmp && rm -rf /opt/sshuttle && mv /tmp/sshuttle-master /opt/sshuttle"
 fi
 
 # rsync
+echo 'checking for solo-utils...'
 if [[ $0 == *'install.sh'* ]] && [ -d ./sdg ]; then
     cd $(dirname $0)
     echo 'uploading local solo-utils...'
     rsync -avz --rsync-path="mkdir -p /opt/sdg && rsync" --progress ./sdg/. root@10.1.1.10:/opt/sdg/.
-    ssh root@10.1.1.10 "ln -s /opt/sdg/sdg /usr/bin/sdg || true" 2> /dev/null
+    ssh root@10.1.1.10 "ln -s /opt/sdg/sdg /usr/bin/sdg || true" 2> /dev/null </dev/null
 else
     # Unpack sdg
-    echo 'uploading solo-utils from source...'
-    ssh root@10.1.1.10 "[[ -d /opt/sdg ]]"
+    ssh root@10.1.1.10 "[[ -d /opt/sdg ]]" 2>/dev/null </dev/null
     if [ $? != 0 ]; then
+        echo 'uploading solo-utils from source...'
         curl -L https://bc0a42b65800ec0dd4c9127dde0cd6e98eb70012:x-oauth-basic@github.com/3drobotics/solodevguide/archive/solo-utils-0.1.0.tar.gz | \
-        ssh root@10.1.1.10 "tar -xvzf - -C /tmp && rm -rf /opt/sdg && mv /tmp/solodevguide-solo-utils-0.1.0 /opt/sdg"
+            ssh root@10.1.1.10 "tar -xvzf - -C /tmp && rm -rf /opt/sdg && mv /tmp/solodevguide-solo-utils-0.1.0 /opt/sdg"
     fi
 fi
 
@@ -29,7 +32,7 @@ install_rpm () {
 
     echo "checking for $name..."
 
-    ssh -o "StrictHostKeyChecking no" -q root@10.1.1.10 "which $name >/dev/null"
+    ssh -o "StrictHostKeyChecking no" -q root@10.1.1.10 "which $name >/dev/null" </dev/null
     if [ $? -ne 0 ]; then
         curl https://s3.amazonaws.com/solo-packages/3.10.17-rt12/$file | ssh -o "StrictHostKeyChecking no" -q root@10.1.1.10 "cat > /tmp/install.rpm; rpm --replacepkgs -i /tmp/install.rpm && echo installed $file"
     fi
@@ -44,4 +47,4 @@ install_rpm "resize2fs" "e2fsprogs-badblocks-1.42.8-r0.cortexa9hf_vfp_neon.rpm"
 install_rpm "mkfs.ext3" "e2fsprogs-mke2fs-1.42.8-r0.cortexa9hf_vfp_neon.rpm"
 install_rpm "lsof" "lsof-4.87-r0.cortexa9hf_vfp_neon.rpm"
 
-echo 'done.'
+echo 'done. solo-utils is installed and up to date.'
