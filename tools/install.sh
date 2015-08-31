07c091332fb3d41900a8a6760388d513204c1e10
Tim Ryan <tim@3drobotics.com>
2015-08-31 14:09:01 -0700

    Adds remote download capability.

--- tools/install.sh
+++ tools/install.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-cd $(dirname $0)
-
 # Unpack sshuttle
+echo 'unpacking sshuttle.'
 ssh root@10.1.1.10 "[[ -d /opt/sshuttle ]]"
 if [ $? != 0 ]; then
     curl -L https://github.com/apenwarr/sshuttle/archive/master.tar.gz | ssh root@10.1.1.10 "tar -xvzf - -C /tmp && rm -rf /opt/sshuttle && mv /tmp/sshuttle-master /opt/sshuttle"
 fi
 
 # rsync
-rsync -avz --rsync-path="mkdir -p /opt/sdg && rsync" --progress ./sdg/. root@10.1.1.10:/opt/sdg/.
-ssh root@10.1.1.10 "ln -s /opt/sdg/sdg /usr/bin/sdg || true" 2> /dev/null
+if [[ $0 == *'install.sh'* ]] && [ -d ./sdg ]; then
+    cd $(dirname $0)
+    echo 'uploading local solo-utils...'
+    rsync -avz --rsync-path="mkdir -p /opt/sdg && rsync" --progress ./sdg/. root@10.1.1.10:/opt/sdg/.
+    ssh root@10.1.1.10 "ln -s /opt/sdg/sdg /usr/bin/sdg || true" 2> /dev/null
+else
+    # Unpack sdg
+    echo 'uploading solo-utils from source...'
+    ssh root@10.1.1.10 "[[ -d /opt/sdg ]]"
+    if [ $? != 0 ]; then
+        curl -L https://bc0a42b65800ec0dd4c9127dde0cd6e98eb70012:x-oauth-basic@github.com/3drobotics/solodevguide/archive/solo-utils-0.1.0.tar.gz | \
+        ssh root@10.1.1.10 "tar -xvzf - -C /tmp && rm -rf /opt/sdg && mv /tmp/solodevguide-solo-utils-0.1.0 /opt/sdg"
+    fi
+fi
 
 install_rpm () {
     name=$1
@@ -24,6 +35,7 @@ install_rpm () {
     fi
 }
 
+echo 'checking rpms...'
 install_rpm "sv" "busybox-1.21.1-r1.cortexa9hf_vfp_neon.rpm"
 install_rpm "parted" "parted-3.1-r1.cortexa9hf_vfp_neon.rpm"
 install_rpm "resize2fs" "libss2-1.42.8-r0.cortexa9hf_vfp_neon.rpm"
--- tools/remote-install.sh
+++
@@ -1,4 +0,0 @@
-# Unpack sshuttle
-if [ ! -d sdg/sshuttle ]; then
-    git clone https://github.com/apenwarr/sshuttle sdg/sshuttle --depth=1
-fi
